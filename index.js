03ad1be8a648a276999ecc357947dcd1d7ef2e33
Prerna <iprernasingh4@gmail.com>
2023-04-21 21:18:24 +0530

    Updated the README file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const qrcode = require('qrcode-terminal');
 
 const { Client } = require('whatsapp-web.js');
@@ -13,20 +14,50 @@ client.on('ready', () => {
 
 //client.on('message', message => {
 //	if(message.body === 'Hi') {
-//		message.reply('hlo');
+//		message.reply('Yes👋');
 //	} else {
-//		message.reply('Buddhu');
+//		message.reply('Or Bolo😁');
 //	}
 //});
+ 
 
 client.on('message', message => {
-	if(message.body === 'Hi') {
-		message.reply('Yes👋');
-	} else {
-		message.reply('Or Bolo😁');
-	}
-});
+	
+    fs.readFile('data.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        } else{
+			message.reply('Or Bolo😁');
+		}
+        
+        // Parse the JSON data
+        const jsonData = JSON.parse(data);
+
+        //get the output from search and send back.
+        const msg = searchByKey(message.body,jsonData);
+        message.reply(msg);
+        
+        
+    });
+    
+
+}); 
+
  
 
 client.initialize();
+
+
+// function for serching data from json file. 
+function searchByKey(key,data) {
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].key.includes(key)) {
+        const valueArray = data[i].value;
+        const randomIndex = Math.floor(Math.random() * valueArray.length);
+        return valueArray[randomIndex];
+        }
+    }
+    return null;
+}
  
